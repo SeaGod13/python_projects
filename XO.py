1f06d0932fb3c91431711e3750abfef344508489
SeaGod13 <smolinez@gmail.com>
2020-03-29 16:35:01 +0300

    pre release

--- XO.py
+++ XO.py
@@ -14,6 +14,44 @@ class Board:
 arg = Board()
 arg.printField()
 print()
-arg.field[1][0] = 'X'
-arg.printField()
+win_X = 0
+win_O = 0
+count = 0
+while win_X == 0 or win_O == 0 or count < 10:
+    win_X = 1
+    count += 1
+    lblx = 'X'
+    lblo = 'O'
+    if count % 2 != 0:
+        move = 'Крестики, '
+        lbl = lblx
+    else:
+        move = 'Нолики, '
+        lbl = lblo
+    print(move + 'введите координаты')
+    print('по горизонтали')
+    x = int(input())
+    if x < 0 or x > 2:
+        print('введите правильные координаты по горизонтали')
+    print('по вертикали')
+    y = int(input())
+    if y < 0 or y > 2:
+        print('введите правильные координаты по вертикали')
+    if arg.field[y][x] != '_':
+        print('!!! введите правильные координаты !!!')
+        print()
+        count -= 1
+    else:
+        arg.field[y][x] = lbl
+    arg.printField()
+    print()
+    if count > 4:
+        if (arg.field[0][0] == arg.field[1][1] and arg.field[1][1] == arg.field[2][2]):
+            if arg.field[0][0] == lblx:
+                print('Победа крестиков')
+                win_X = 1
+            elif arg.field[0][0] == lblo:
+                print('Победа ноликов')
+                win_O = 1
+
 
