26c3cb81eff4fb223a85b5e78b9f184178e15aa0
SeaGod13 <smolinez@gmail.com>
2020-04-01 09:33:47 +0300

    final release

--- XO.py
+++ XO.py
@@ -11,41 +11,55 @@ class Board:
                 print(self.field[i][j], end=' ')
             print()
 
-    def win_check(self,player):
-        flag = True
-        for i in range(self.size-1):
-            for j in range(self.size-1):
-                if arg.field[i][j] != arg.field[i + 1][j + 1]:
-                    flag = False
-                elif arg.field[i][j] != arg.field[i][j + 1]:
-                    flag = False
-                elif arg.field[j][i] != arg.field[j][i+1]:
-                    flag = False
-        return flag
-
-    # def win_check(self, player):
-    #     win = ''
-    #     i = 0
-    #     win_count = 0
-    #     while i < self.size - 1:
-    #         if arg.field[i][i] == arg.field[i+1][i+1]: # 00-22
-    #             win_count += 1
-    #         if player == 'X' and win_count == self.size - 1:
-    #             win = 'X'
-    #         elif player == 'O' and win_count == self.size - 1:
-    #             win = 'O'
-    #         i += 1
-    #     return win
-
-        # if (arg.field[0][0] == arg.field[1][1] and arg.field[1][1] == arg.field[2][2]): # 00 - 22
-        #     game_over = True
-        # elif (arg.field[0][0] == arg.field[0][1] and arg.field[0][1] == arg.field[0][2]): # 00 - 02
-        #     game_over = True
-        # elif (arg.field[0][0] == arg.field[1][0] and arg.field[1][0] == arg.field[2][0]): #
-        #     game_over = True
-        # elif (arg.field[0][2] == arg.field[1][1] and arg.field[1][1] == arg.field[2][0]):
-        #     game_over = True
-
+    def win_check(self, player):
+        # первая проверка диагональ 00 -- 22
+        flag = False
+        cnt = 0
+        while cnt == 0:
+            for i in range(self.size-1):
+                if arg.field[i][i] == arg.field[i+1][i+1]:
+                    flag = True
+                    cnt += 1
+            if cnt == self.size - 1 and flag:
+                cnt = 1
+                return flag
+            break
+        # вторая проверка диагональ 02 -- 20
+        flag = False
+        cnt = 0
+        while cnt == 0:
+            for i in range(self.size - 1):
+                if arg.field[i][self.size - 1 - i] == arg.field[i + 1][self.size - 1 - i - 1]:
+                    flag = True
+                    cnt += 1
+            if cnt == self.size - 1 and flag:
+                cnt = 1
+                return flag
+            break
+        # третья проверка горизонтали
+        for i in range(self.size):
+            flag = False
+            cnt = 0
+            for j in range(self.size - 1):
+                # a = arg.field[i][j]
+                # b = arg.field[i][j+1]
+                if arg.field[i][j] == arg.field[i][j+1] != '_':
+                    flag = True
+                    cnt += 1
+            if cnt == self.size - 1 and flag:
+                return flag
+        # червертая проверка вертикали
+        for j in range(self.size):
+            flag = False
+            cnt = 0
+            for i in range(self.size - 1):
+                # a = arg.field[i][j]
+                # b = arg.field[i+1][j]
+                if arg.field[i][j] == arg.field[i+1][j] != '_':
+                    flag = True
+                    cnt += 1
+            if cnt == self.size - 1 and flag:
+                return flag
 
 arg = Board()
 arg.printField()
